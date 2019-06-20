53843d3ec1dd6557bae3ca280681168877e10a22
유재희 <wongirl111@gmail.com>
2019-06-21 08:36:16 +0900

    gravity

---
+++ NanasReunion/app/src/main/java/yjj/nanasreunion/Components/Item/GravityUp.java
@@ -0,0 +1,59 @@
+package yjj.nanasreunion.Components.Item;
+
+import android.graphics.Canvas;
+import android.graphics.Rect;
+
+import yjj.nanasreunion.Components.Camera;
+import yjj.nanasreunion.Components.Graphics.Graphics;
+import yjj.nanasreunion.Components.Graphics.SpriteGraphics;
+import yjj.nanasreunion.Components.Physics.Physics;
+import yjj.nanasreunion.Objects.Pawn;
+import yjj.nanasreunion.R;
+import yjj.nanasreunion.Services.ServiceHub;
+import yjj.nanasreunion.Services.Timer;
+import yjj.nanasreunion.Vector2f;
+
+public class GravityUp extends Item {
+    private Graphics original_graphics;
+    private Graphics graphics;
+    private Vector2f pivot;
+
+    protected GravityUp()
+    {
+        super("GravityUp", 5.f);
+    }
+
+    @Override
+    public Item Create()  {
+        return new GravityUp();
+    }
+
+    @Override
+    public void Use(Pawn pawn, Camera camera) {
+        original_graphics = pawn.graphics;
+        graphics     = new SpriteGraphics(ServiceHub.Inst().GetBitmap(R.drawable.gravityup),
+                10,1,1);
+        graphics.SetScale(0.1f, 0.1f);
+        pivot = new Vector2f(0.5f, 0.5f);
+
+        Timer.SetTimeDilation(0.5f);
+    }
+
+    @Override
+    public boolean UpdateAndValidate(Pawn pawn, Camera camera, float deltaTime) {
+        return super.UpdateAndValidate(pawn, camera, deltaTime);
+    }
+
+    @Override
+    public void Stop(Pawn pawn, Camera camera) {
+        pawn.graphics = original_graphics;
+        Timer.SetTimeDilation(1.f);
+    }
+
+    @Override
+    public void Draw(Canvas canvas, Camera camera, Pawn pawn) {
+        Vector2f position     = new Vector2f(pawn.position.x - 0.1f, pawn.position.y + 0.5f);
+
+        graphics.Draw(canvas, camera, position, pivot, 0.f);
+    }
+}
